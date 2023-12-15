df6e6a180fee4a4f6238f498565a437d662e898b
Adrian-finstad <adrian.finstad@hotmail.com>
2023-12-15 15:23:27 +0100

    Add project files.

---
+++ Bossfight/GameCharacter.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bossfight
+
+{
+    internal class GameCharacter
+    {
+        public int Health;
+        public int Strength;
+        public int Stamina;
+        public string Name;
+        public int MaxStamina;
+
+        public GameCharacter(int health, int strength, int stamina, string name)
+        {
+
+            Health = health;
+            Strength = strength;
+            Stamina = stamina;
+            MaxStamina = stamina;
+            Name = name;
+        }
+
+        public static void Fight(GameCharacter Hero, GameCharacter Boss)
+        {
+            if (Hero.Health > 0 && Hero.Stamina > 0 && Boss.Health > 0 && Boss.Stamina > 0)
+            {
+                Boss.Health -= Hero.Strength;
+                Boss.Stamina -= 10;
+                Hero.Health -= Boss.Strength;
+                Hero.Stamina -= 10;
+
+                if (Boss.Health <= 0)
+                {
+                    Console.WriteLine("The hero killed the boss! Congratulations! ");
+                }
+                else if(Hero.Health <= 0)
+                Console.WriteLine($"The Boss killed your hero. You suck. Get your ass to NAV.");
+                else if (Boss.Health > 0 && Hero.Health > 0)
+                {
+                    Console.WriteLine($"{Boss.Name} did {Boss.Strength} damage to the {Hero.Name}!{Hero.Name}Now has {Hero.Health} health left. {Boss.Name} now has {Boss.Stamina} stamina left.");
+                    Console.WriteLine($"{Hero.Name} did {Hero.Strength} damage to the {Boss.Name}!{Boss.Name}Now has {Boss.Health} health left. {Hero.Name} now has {Hero.Stamina} stamina left.");
+
+                    if (Hero.Stamina <= 0)
+                    {
+                        Console.WriteLine("Your hero is out of stamina, and needs to recharge!");
+                    }
+                    else if (Boss.Stamina <= 0)
+                    {
+                        Console.WriteLine("The boss is out of stamina, and needs to recharge!");
+                    }
+                    
+                }
+                
+                
+            }
+        }
+
+        public void Recharge()
+        {
+            Stamina = MaxStamina;
+        }
+
+
+
+    }
+}
---
+++ Bossfight/Program.cs
@@ -0,0 +1,39 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+//HVORDAN LAGE RANDOM
+//Random rand = new Random();
+//var randomNumber = rand.Next(0, 3);
+
+using Bossfight;
+Console.WriteLine("Welcome to the bossfight!");
+Random rand = new Random();
+//var BossStrength = rand.Next(0, 31);
+var Hero = new GameCharacter(100, 20, 40, "Herkules");
+var Boss = new GameCharacter(400, 0, 10, "Erna Solberg");
+void main()
+{
+    Console.WriteLine("Press 1 to fight!");
+    Console.WriteLine("Press 2 to recharge Hero stamina");
+    Console.WriteLine("Press 3 to recharge boss stamina. ");
+    while (true) {
+        Boss.Strength = rand.Next(0, 31);
+        var playerChoice = Console.ReadLine();
+        if (playerChoice == "1") {
+            GameCharacter.Fight(Hero, Boss);
+            main();    
+        }
+        else if(playerChoice == "2")
+        {
+            Hero.Recharge();
+        }
+        else if(playerChoice == "3")
+        {
+            Boss.Recharge();
+        }
+        
+    }
+}
+
+main();
+
+
